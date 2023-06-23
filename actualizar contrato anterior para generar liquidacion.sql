5599e83d3e840223b84863b9b756349fb90fd81a
ZeroJChang <imbochfckyeah@gmail.com>
2023-06-23 16:34:56 -0600

    new changes

---
+++ actualizar contrato anterior para generar liquidacion.sql
@@ -0,0 +1,8 @@
+SELECT * FROM PINULITO_NOMINA..tEmpleado WHERE codEmpleado = 0970
+SELECT * FROM PINULITO_NOMINA..tContrato WHERE codEmpleado = 0970
+SELECT * FROM PINULITO_NOMINA..tDepartamento WHERE codDepto = 306
+SELECT * FROM PINULITO_NOMINA..tEmpresa WHERE codEmpresa = 3
+
+--UPDATE PINULITO_NOMINA..tEmpleado set noContract = '43842' WHERE codEmpleado = 0970
+--UPDATE PINULITO_NOMINA..tContrato set finContract = '1900-01-01' WHERE noContract = 43660
+--fecha anterior : 2023-06-16, noContract : 43842
--- validar tiendas actualizadas y solventes de informacion.sql
+++ validar tiendas actualizadas y solventes de informacion.sql
@@ -5,13 +5,23 @@ select * from PINULITO_PDV..tDispositivoTienda where versionApp = '6.0.18' and y
 select * from PINULITO_PDV..tDispositivoTienda where versionApp != '6.0.18' and year(ultimaConexionPOS) = 2023
 
 -- consultar por tienda
-select * from PINULITO_PDV..tTienda where tda_nombre like '%justo%'
+select * from PINULITO_PDV..tTienda where tda_nombre like '%jutiapa 5%'
 
 -- ver cuantas tiendas ya llegaron, por empresa
 select DISTINCT(tienda) from PINULITO_PDV..tFacturaTemporalPos where empresa = '00001'
 
 -- consultar cuantas facturas por empresa, tienda y fecha
-select count(*) from PINULITO_PDV..tFacturaTemporalPos where tienda = '00071' and empresa = '00001' and convert(date,fechaHora)='2023-06-13'
+select count(*) from PINULITO_PDV..tFacturaTemporalPos where tienda = '00112' and empresa = '00003' and convert(date,fechaHora)='2023-06-12'
 
 -- consultar cuantas facturas por empresa, tienda y fecha [VER DETALLE]
-select * from PINULITO_PDV..tFacturaTemporalPos where empresa = '00001' and tienda = '00071' and convert(date,fechaHora)='2023-06-13'
+select SUM(total) from PINULITO_PDV..tFacturaTemporalPos where empresa = '00003' and tienda = '00112' and convert(date,fechaHora)='2023-06-13'
+
+-- consultar cuantas factura cargo una tienda
+SELECT T.tienda, T.empresa, T.tda_nombre, (SELECT COUNT(*) FROM PINULITO_PDV..tFacturaTemporalPos F WHERE F.tienda = T.tienda AND F.empresa = T.empresa and convert(date, F.fechaHora) = '2023-06-12') AS TotalFacturas
+FROM PINULITO_PDV..tTienda T where inactiva = 0 order by TotalFacturas asc
+
+-- consultar tiendas actualizadas
+SELECT T.tienda, T.empresa, T.tda_nombre,
+       (SELECT COUNT(*) FROM PINULITO_PDV..tFacturaTemporalPos F
+        WHERE F.tienda = T.tienda AND F.empresa = T.empresa and convert(date, F.fechaHora) = '2023-06-12') AS TotalFacturas
+FROM PINULITO_PDV..tTienda T where inactiva = 0 and empresa not in(00015,00018) and T.tda_nombre like '%JUTIAPA 5%' order by TotalFacturas asc
